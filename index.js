ab2371bcd75ad1653268995534c04428fa0dfa1c
LisaCR01 <lisagunn01@gmail.com>
2022-03-20 18:25:33 +0000

    written initial choices question in js using case and switch

--- index.js
+++ index.js
@@ -25,3 +25,55 @@ const db = mysql.createConnection(dbOptions);
 app.listen(PORT, () => {
       console.log(`Server running on port http://localhost:${PORT}`);
     });
+
+init();
+
+// When the command line is run computer asks initialQuestions
+function initialQuestions() {
+    prompt([{
+        type:"list",
+        name: "seeWho",
+        message: "Which employee group would you like to see?",
+        choices: [
+        "Everyone",
+        "All departments",
+        "All managers",
+        "All roles",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an Employee",
+        "All done"
+        
+    ]}])
+    .then(res => {
+        let choice = res.seeWho;
+        switch (choice) {
+        case "Everyone": console.log("that's everyone"); 
+        return initialQuestions();
+        case "All departments": console.log("view all departments")
+        return initialQuestions();
+        case "All managers": console.log("view all managers")
+        return initialQuestions();
+        case "All roles": console.log("view all roles")
+        return initialQuestions();
+        case "Add a department": console.log("add a department")
+        return initialQuestions();
+        case "Add a role": console.log("add a role")
+        return initialQuestions();
+        case "Add an employee": console.log("add an employee")
+        return initialQuestions();
+        default: allDone();
+}})};
+
+// When the user has finished selecting reviewing their company it will exit initialQuestions.
+function allDone() {
+    console.log("All done!");
+    process.exit();
+  }
+
+// init initialises setup.
+function init() {
+    printMessage(["Welcome!", "Let\'s organise the company."]);
+    initialQuestions();
+  };
