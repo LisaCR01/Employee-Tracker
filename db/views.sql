795d6835780c7ad896cdad168494e1eaf495f951
LisaCR01 <lisagunn01@gmail.com>
2022-04-16 12:42:16 +0100

    added titles for screenshots

--- db/views.sql
+++ db/views.sql
@@ -10,7 +10,7 @@ FROM employee_names
 LEFT JOIN roles ON employee_names.roles = roles.role_id
 LEFT JOIN manager ON employee_names.manager = manager.man_id;
 
- CREATE VIEW employee_all AS
+CREATE VIEW employee_all AS
 SELECT first_name,last_name,job,leader, department.dept,salary
 From employee_interim
 LEFT join department  on employee_interim.department=department.dept_id; 
